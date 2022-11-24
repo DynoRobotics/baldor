82bd75a93fc3f64885b8c91e2895c832cd6b6358
Erik Örjehag <erik@orjehag.se>
2022-11-24 08:28:45 +0100

    Humble port

--- setup.py
+++ setup.py
@@ -10,7 +10,25 @@ setup(
         author='Francisco Suarez-Ruiz',
         author_email='fsuarez6@gmail.com',
         url='http://wiki.ros.org/baldor',
-        packages=['src/' + package_name],
+        packages=[package_name],
+        data_files=[
+            ('share/' + package_name, ['package.xml']),
+            ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+            ],
+        )
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+package_name='baldor'
+setup(
+        name=package_name,
+        version='0.1.3',
+        description='The baldor package',
+        author='Francisco Suarez-Ruiz',
+        author_email='fsuarez6@gmail.com',
+        url='http://wiki.ros.org/baldor',
+        packages=[package_name],
         data_files=[
             ('share/' + package_name, ['package.xml']),
             ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
